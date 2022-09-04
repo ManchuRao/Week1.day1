0357f2fdf0f4539c43799e23a845c185617d26ea
ManchuRao <112795623+ManchuRao@users.noreply.github.com>
2022-09-04 13:31:13 +0530

    Add files via upload

---
+++ PrimeNo.java
@@ -0,0 +1,23 @@
+package week1.day1;
+
+public class PrimeNo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num=6;
+	
+		for(int i=2;i<=num-1;i++) {
+		if(num%1==0) {
+		
+			System.out.println("this is prime number");
+		}
+	
+	else {
+			System.out.println(" Not prime number ");
+	}
+}
+	}
+}
+
+
+
